439a6ca82abffad6beaa95ed99a6bddebfa36498
hscokies <hello@hscokies.xyz>
2026-05-20 11:09:18 +0500

    feat(independent work 2): complete fourth task

---
+++ independent-work/app/src/main/java/com/example/second_task/ui/screens/ViewContacts.kt
@@ -0,0 +1,62 @@
+package com.example.second_task.ui.screens
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.first_task.R
+import com.example.second_task.ui.screens.ui.theme.IndependentWorkTheme
+
+class ViewContacts : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ViewContactsPage()
+        }
+    }
+}
+
+@Composable
+fun ViewContactsPage() {
+    // Material icons are now deprecated
+    // https://stackoverflow.com/a/79787821
+    IndependentWorkTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize().padding(innerPadding)) {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = stringResource(R.string.icon),
+                    modifier = Modifier.size(100.dp)
+                )
+                Text(text = stringResource(R.string.view_contacts_title))
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ViewContactsPreview() {
+    ViewContactsPage()
+}
